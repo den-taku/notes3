a32862d865fe00ed58f6a8023a8f23e871cc7cb8
dentaku <dentaku1412@gmail.com>
2021-08-14 00:44:08 +0900

    See .

---
+++ actix_web_tutorial/hello-world/templates/server.rs
@@ -0,0 +1,21 @@
+use actix_web::{get, App, HttpRequest, HttpServer, Responder};
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+
+#[get("/")]
+async fn index(_req: HttpRequest) -> impl Responder {
+    "Welcome!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    builder
+        .set_private_key_file("key.pem", SslFiletype::PEM)
+        .unwrap();
+    builder.set_certificate_chain_file("filcert.peme").unwrap();
+
+    HttpServer::new(|| App::new().service(index))
+        .bind_openssl("127.0.0.1:8080r", builder)?
+        .run()
+        .await
+}
