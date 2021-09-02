613db2c58d5f15d66ad49746096c309c4de46f3e
dentaku <dentaku1412@gmail.com>
2021-09-03 00:31:25 +0900

    See error of double

---
+++ test.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+#include <stdio.h>
+
+int main() {
+    std::cout << ((0.6 - 0.0) <= 0.6) << std::endl;
+    std::cout << ((1.6 - 1.0) <= 0.6) << std::endl;
+    std::cout << std::fixed << std::setprecision(50) << (1.6 - 1.0)  << std::endl;
+    std::cout << ((2.6 - 2.0) <= 0.6) << std::endl;
+    std::cout << std::fixed << std::setprecision(30) << (2.6 - 2.0)  << std::endl;
+    std::cout << ((3.6 - 3.0) <= 0.6) << std::endl;
+    std::cout << ((4.6 - 4.0) <= 0.6) << std::endl;
+    std::cout << std::fixed << std::setprecision(30) << (4.6 - 4.0)  << std::endl;
+    std::cout << ((5.6 - 5.0) <= 0.6) << std::endl;
+    std::cout << ((6.6 - 6.0) <= 0.6) << std::endl;
+    std::cout << ((7.6 - 7.0) <= 0.6) << std::endl;
+    std::cout << ((8.6 - 8.0) <= 0.6) << std::endl;
+    std::cout << ((9.6 - 9.0) <= 0.6) << std::endl;
+    std::cout << ((10.6 - 10.0) <= 0.6) << std::endl;
+    std::cout << ((11.6 - 11.0) <= 0.6) << std::endl;
+    std::cout << ((12.6 - 12.0) <= 0.6) << std::endl;
+    std::cout << ((13.6 - 13.0) <= 0.6) << std::endl;
+    std::cout << ((14.6 - 14.0) <= 0.6) << std::endl;
+    std::cout << ((15.6 - 15.0) <= 0.6) << std::endl;
+    std::cout << ((16.6 - 16.0) <= 0.6) << std::endl;
+    std::cout << std::fixed << std::setprecision(30) << (16.6 - 16.0)  << std::endl;
+}
